30c290c67e21cf9b13cda7e8bd3873e3e8be27e1
klldmeprotonme <>
2022-09-08 14:17:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+import os
+import sys
+import uvloop
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+env_vars = {}
+if os.path.isfile('.device'):
+    with open('.device') as f:
+        for line in f:
+            if line.startswith('#') or not line.strip():
+                continue
+            key, value = line.strip().split('=', 1)
+            env_vars[key] = value.replace("\"", "")
+else:
+  print("error device in the file")
+  sys.exit()
+
+os.system('pip install -U klldFNv3')
+os.system('clear')
+
+import klldFNv3
+
+
+client = klldFNv3.PartyBot(
+  device_id=env_vars['DEVICE_ID'],
+  account_id=env_vars['ACCOUNT_ID'],
+  secret=env_vars['SECRET']
+)
+
+try:
+    client.run()
+except Exception as e:
+    print(e)
+    print("Can't login because your device auths is probably wrong.")
